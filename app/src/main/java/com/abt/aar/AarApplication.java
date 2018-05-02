6c6f0e2c520414ff7a40a5d328c54d74a323ab9a
huangweiqi <askviky2010@gmail.com>
2018-05-03 00:11:52 +0800

    添加Middle-release

---
+++ app/src/main/java/com/abt/aar/AarApplication.java
@@ -0,0 +1,15 @@
+package com.abt.aar;
+
+import com.abt.basic.app.BasicApplication;
+
+/**
+ * Created by huangweiqi on 03/05/2018.
+ */
+public class AarApplication extends BasicApplication {
+
+    @Override
+    public void initComplete() {
+
+    }
+
+}
--- app/src/main/java/com/abt/aar/MainActivity.java
+++ app/src/main/java/com/abt/aar/MainActivity.java
@@ -3,16 +3,12 @@ package com.abt.aar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.abt.library.Lib;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Lib.print();
 
     }
 }
